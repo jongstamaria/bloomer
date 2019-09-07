ad814cbfc8a156bc27b86b9d0d6abb91f3c8284f
jongstamaria <49810107+jongstamaria@users.noreply.github.com>
2019-09-07 23:18:37 +0800

    Update bloomerapi.py

--- bloomerapi.py
+++ bloomerapi.py
@@ -1,29 +1,7 @@
-from flask import Flask, request, jsonify, url_for
-import pymysql, pymysql.cursors, dbconnect
-import simplejson as json
-
+from flask import Flask
 app = Flask(__name__)
-app.config["DEBUG"] = True
-application = app # our hosting requires application in passenger_wsgi
-
-#--- display tour_trips base on google place id ----#
-@app.route('/menucategories', methods=['GET'])
-def menucategories():
-
-    if 'id' in request.args:
-        id = request.args['id']
-    else:
-        return "Error: No id field provided. Please specify an id."
-    
-    try:
-        conn = dbconnect.getConnection()
-        cur = conn.cursor()
-        cur.execute("SELECT * 
-            FROM menu_categories 
-            WHERE client_id = %s", id)
-        result = cur.fetchall()
-        #return jsonify(result)
-        return json.dumps(result)
-        
-    except Exception as e:
-        return(str(e))
+@app.route('/')
+def hello_world():
+    return 'Hello, Heroku World!'
+if __name__ == '__main__':
+    app.run()
